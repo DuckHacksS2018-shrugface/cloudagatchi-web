f662344ff7fa3fcb8bb097d902352682cba69e33
U-DESKTOP-JI2C39O\carly <carlypalicz@gmail.com>
2018-03-25 02:56:23 -0400

    carly's changes

---
+++ src/Actions.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class Actions extends Component {
+
+/*rons main thing will call this class which returns a bunch of html */	
+  render() {
+    return (
+      <div className="App">
+       	<button type="button">Feed</button>
+	<button type="button">Play</button>
+	<button type="button">Clean</button>
+	<button type="button">Bathe</button>
+	<button type="button">Scold</button>
+	<button type="button">Heal</button>
+	
+      </div>
+    );
+  }
+}
+
+export default Actions;
